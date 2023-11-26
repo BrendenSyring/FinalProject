c3e26f7b9887d2b07d049f3a562f9e6e7875fef4
Brenden S <s0544317@student.rockvalleycollege.edu>
2023-11-26 14:18:41 -0600

    Added some small fixes

--- FinalProject/ContentView.swift
+++ FinalProject/ContentView.swift
@@ -2,67 +2,75 @@ import SwiftUI
 
 struct ContentView: View {
     let names = [
-        "Akshan", "Poppy", "Singed", "Maokai", "Quinn", "Vayne", "Olaf", "Ornn", "Camille", "Rengar", "Nasus",
+        "Akshan: "
+        , "Poppy", "Singed", "Maokai", "Quinn", "Vayne", "Olaf", "Ornn", "Camille", "Rengar", "Nasus",
         "Kled", "Zac", "Malphite", "Warwick", "Kayle", "Tryndamere", "Shen", "Fiona", "Urgot", "K’Sante", "Illaoi",
         "Sylas", "Cho’Gath", "Garen", "Irelia", "Gwen", "Jax", "Dr.Mundo", "Darius", "Tahm Kench", "Wukong", "Teemo",
         "Riven", "Vladimir", "Pantheon", "Aatrox", "Udyr", "Kennen", "Gnar", "Volibear", "Gangplank", "Mordekaiser",
-        "Yone", "Renekton", "Rumble", "Alkali", "Sion", "Gragas", "York", "Jayce", "Trundle", "Yasuo", "Shri", "Graves"
+        "Yone", "Renekton", "Rumble", "Alkali", "Sion", "Gragas", "Yorick", "Jayce", "Trundle", "Yasuo", "Ahri", "Graves"
+        
     ]
 
     @State private var selectedName: String = ""
-        @State private var confirmationMessage: String = ""
-        @State private var isNameConfirmed: Bool = false
+    @State private var confirmationMessage: String = ""
+    @State private var isNameConfirmed: Bool = false
 
-        var body: some View {
-            NavigationView {
-                VStack {
-                    Text("Top Counters")
-                    Text("Enter in a Top Lane champion")
+    var body: some View {
+        NavigationView {
+            VStack {
+                Text("Top Counters")
+                    .fontWeight(.bold)
+                    .font(.largeTitle)
+                    .foregroundColor(Color.black)
+                Text("Enter in a Top Lane champion")
+                Link("List of Champions", destination: URL(string: "https://u.gg/lol/top-lane-tier-list")!)
+                    .foregroundColor(.blue)
+                    .font(.headline)
+                Image("default_image")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 100, height: 100)
+                    .padding()
+                TextField("Select a name", text: $selectedName)
+                    .padding()
+                    .autocapitalization(.none)
+                    .disableAutocorrection(true)
 
-                    TextField("Select a name", text: $selectedName)
-                        .padding()
-                        .autocapitalization(.none)
-                        .disableAutocorrection(true)
-
-                    NavigationLink(destination: isNameConfirmed ? AnotherView() : nil, isActive: $isNameConfirmed) {
-                        Button(action: {
-                            self.confirmSelection()
-                        }) {
-                            Text("Confirm")
-                                .padding()
-                                .foregroundColor(.white)
-                                .background(Color.blue)
-                                .cornerRadius(10)
-                        }
+                NavigationLink(destination: AnotherView(selectedName: $selectedName, isNameConfirmed: $isNameConfirmed, confirmationMessage: $confirmationMessage), isActive: $isNameConfirmed) {
+                    Button(action: {
+                        self.confirmSelection()
+                    }) {
+                        Text("Confirm")
+                            .padding()
+                            .foregroundColor(.white)
+                            .background(Color.blue)
+                            .cornerRadius(10)
                     }
-                    .background(Color.clear)
-                    .navigationBarHidden(true)
-
-                    Link("List of Champions", destination: URL(string: "https://u.gg/lol/top-lane-tier-list")!)
-                        .foregroundColor(.blue)
-                        .font(.headline)
-
-                    Text(confirmationMessage)
-                        .padding()
-                        .foregroundColor(.green)
                 }
-                .padding()
-                .navigationBarTitle("Champion Selector", displayMode: .inline)
-            }
-        }
+                .background(Color.clear)
+                .navigationBarHidden(true)
 
-        private func confirmSelection() {
-            if names.contains(selectedName) {
-                confirmationMessage = "Confirmed: \(selectedName)"
-                isNameConfirmed = true
-            } else {
-                confirmationMessage = "Invalid selection"
+                Text(confirmationMessage)
+                    .padding()
+                    .foregroundColor(.green)
             }
+            .padding()
+            .navigationBarTitle("Champion Selector", displayMode: .inline)
         }
     }
 
-    struct ContentView_Previews: PreviewProvider {
-        static var previews: some View {
-            ContentView()
+    private func confirmSelection() {
+        if names.contains(selectedName) {
+            confirmationMessage = " \(selectedName) is a valid selection"
+            isNameConfirmed = true
+        } else {
+            confirmationMessage = "Invalid selection"
         }
     }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
--- FinalProject/MyAppView.swift
+++ FinalProject/MyAppView.swift
@@ -1,20 +1,20 @@
 import SwiftUI
 
 struct AnotherView: View {
+    @Binding var selectedName: String
+    @Binding var isNameConfirmed: Bool
+    @Binding var confirmationMessage: String
+
     var body: some View {
         VStack {
             Spacer()
-            Text("Yippee")
+            Text("You picked \(selectedName).")
                 .font(.largeTitle)
                 .fontWeight(.bold)
+                
+                
             Spacer()
         }
         .navigationBarTitle("Confirmed", displayMode: .inline)
-    }
-}
-
-struct AnotherView_Previews: PreviewProvider {
-    static var previews: some View {
-        AnotherView()
     }
 }
